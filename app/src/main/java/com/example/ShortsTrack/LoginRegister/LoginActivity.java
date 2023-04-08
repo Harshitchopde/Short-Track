2f0f2c1d1939061263fe334929a37acbf17fb084
Harshitchopde <21bsm025@iiitdmj.ac.in>
2023-04-08 14:18:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/ShortsTrack/LoginRegister/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.example.ShortsTrack.LoginRegister;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ShortsTrack.MainActivity;
+
+
+
+import com.example.ShortsTrack.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private TextView txtSignIn;
+    private EditText use_email_enter,use_password_enter;
+    private MaterialButton sign_in;
+    private ProgressBar progressBar;
+    private
+    FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
+ActivityLoginBinding binding;
+    FirebaseDatabase database;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+       initvariable();
+        sign_in.setOnClickListener(view -> {
+            final  String User_email = use_email_enter.getText().toString().trim();
+            final  String User_pswd = use_password_enter.getText().toString().trim();
+
+            if (TextUtils.isEmpty(User_email)){
+                use_email_enter.setError("Email is Require");
+                return;
+            }
+            if(TextUtils.isEmpty(User_pswd)){
+                use_password_enter.setError("Password is Require");
+                return;
+            }
+
+
+            if (User_pswd.length()<6){
+                use_password_enter.setError("Password must be greater than 6 char ");
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(User_email,User_pswd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public
+                void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "User Login Successfull", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "error"+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        });
+
+
+
+        txtSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private
+    void initvariable() {
+        txtSignIn=binding.txtSignIn;
+
+
+        use_password_enter=binding.edtSignInPassword;
+        use_email_enter = binding.edtSignInEmail;
+        sign_in = binding.btnSignIn;
+        progressBar = binding.signInProgressBar;
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();;
+    }
+}
---
+++ app/src/main/java/com/example/ShortsTrack/LoginRegister/SignUpActivity.java
@@ -0,0 +1,120 @@
+package com.example.ShortsTrack.LoginRegister;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ShortsTrack.MainActivity;
+
+import com.example.ShortsTrack.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+
+   private TextView txtSignUp;
+   private EditText use_name,use_email,use_password1,use_cmpassword;
+   private MaterialButton sign_up;
+   private ProgressBar progressBar;
+    private
+    ActivitySignUpBinding binding;
+    FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
+
+    FirebaseDatabase database;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initVariable();
+
+        if (firebaseUser!=null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+        sign_up.setOnClickListener(view -> {
+            final  String User_name = use_name.getText().toString().trim();
+            final  String User_email = use_email.getText().toString().trim();
+            final  String User_cmpswd = use_cmpassword.getText().toString().trim();
+            final  String User_pswd = use_password1.getText().toString().trim();
+            if (TextUtils.isEmpty(User_name)){
+                use_name.setError("Name is Require");
+                return;
+            }
+            if (TextUtils.isEmpty(User_email)){
+                use_email.setError("Email is Require");
+                return;
+            }
+            if(TextUtils.isEmpty(User_pswd)){
+                use_password1.setError("Password is Require");
+                return;
+            }
+            if(TextUtils.isEmpty(User_cmpswd)){
+                use_cmpassword.setError("Password is Require");
+                return;
+            }
+            if (!(User_pswd.equals(User_cmpswd))){
+                use_cmpassword.setError("not match with above");
+                use_cmpassword.setText("");
+                return;
+            }
+            if (User_pswd.length()<6){
+                use_password1.setError("Password must be greater than 6 char ");
+                return;
+            }
+            mAuth.createUserWithEmailAndPassword(User_email,User_cmpswd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public
+                void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(SignUpActivity.this, "User Register Successfull", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                    }
+                    else{
+                        Toast.makeText(SignUpActivity.this, "error"+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        });
+
+
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private
+    void initVariable() {
+        txtSignUp = binding.txtSignUp;
+        use_name=binding.edtSignUpFullName;
+        use_password1=binding.edtSignUpPassword;
+        use_cmpassword = binding.edtSignUpConfirmPassword;
+        use_email = binding.edtSignUpEmail;
+        sign_up = binding.btnSignUp;
+        progressBar = binding.signUpProgressBar;
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();;
+    }
+}
